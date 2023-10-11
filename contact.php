0101a2f80c7eb38f23c86742a1192d5f53ca5d14
mikeS759 <mikeparker759@gmail.com>
2023-10-11 12:24:11 -0700

    second

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main class="contact=main">
+        <!--editiable contact location, mailing, administrator section along with contact form-->
+    </main>
+</body>
+</html>
---
+++ facility_grounds.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body> 
+    <main class="facility-grounds-main">
+        <h2>Our Facility</h2>
+        <div>
+
+        </div>
+    </main>
+</body>
+</html>
