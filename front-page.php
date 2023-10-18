ee873454f8f23862e70dd472df196f9c0795908b
Olive Chija <olive171512@gmail.com>
2023-10-18 15:52:19 -0600

    front-page edit

---
+++ front-page.php
@@ -0,0 +1,85 @@
+<?php
+/*
+Template Name: Custom Home Template
+*/
+
+get_header(); ?>
+
+<main id="primary" class="site-main">
+
+    <h1>Home</h1>
+    <section class="home-intro">
+        <!--background image -->
+        <h1>Home</h1>
+        <section class="home-intro">
+            <!--background image -->
+            <!DOCTYPE html>
+            <html lang="en">
+
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+            </head>
+
+            <body>
+                <h1>Home</h1>
+
+
+
+                <section class="home-intro">
+
+                    <h2>Little Bits therapeutic Riding association</h2>
+                    <p>Little Bits Therapeutic Riding Association, We have been serving Edmonton since 1975.Each year,
+                        up to 195 riders benefit from our winter, spring and fallprogram and summer riding week.</p>
+                </section>
+                <section>
+                    <h2>Program</h2>
+                    <div class="">
+
+                        <!-- images to be added in the cards -->
+
+                        <h3>Horse Back Riding</h3>
+                        <p>Experience the thrill of horseback riding at our facility, where every hoofbeat is a symphony
+                            of adventure and every ride is a journey into the heart of excitement!</p>
+                        <button><a href="program_profile.php">Read more</a></button>
+                    </div>
+
+
+                    <!-- images to be added in the cards -->
+
+                    <div class="">
+
+                        <h3>New experiences </h3>
+                        <p>Experience the thrill of horseback riding at our facility, where every hoofbeat is a symphony
+                            of adventure and every ride is a journey into the heart of excitement!</p>
+                        <button><a href="program_profile.php">Read more</a></button>
+                    </div>
+                    <div class="">
+
+                        <!-- images to be added in the cards -->
+                        <h3>volunteer</h3>
+                        <p>At our facility, we believe in the power of volunteers who generously dedicate their time and
+                            passion to make a difference in the lives of kids in need.</p>
+                        <button><a href="program_profile.php">Read more</a></button>
+                    </div>
+                </section>
+                <section class="reviews">
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </section>
+</main>
+<?php
+get_footer();
+?>
+</body>
+
+</html>
